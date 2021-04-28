d7f096bdeeec134615b1c65b03196f0b0909ad85
Sofia Calderon <sofia.ester.calderon@gmail.com>
2021-04-28 15:28:17 +1000

    added all tests for apiConverts core

--- src/utils/ADempiere/apiConverts/core.js
+++ src/utils/ADempiere/apiConverts/core.js
@@ -56,199 +56,76 @@ function convertCurrency(currency) {
       costingPrecision: 0
     }
   }
-  const convertedCUrrency = camelizeObjectKeys(currency)
-  renameObjectKey(convertedCUrrency, 'isoCode', 'iSOCode')
-  renameObjectKey(convertedCUrrency, 'currencySymbol', 'curSymbol')
-  return convertedCUrrency
+  const convertedCurrency = camelizeObjectKeys(currency)
+  renameObjectKey(convertedCurrency, 'isoCode', 'iSOCode')
+  renameObjectKey(convertedCurrency, 'currencySymbol', 'curSymbol')
+  return convertedCurrency
 }
 
 export function convertBusinessPartner(businessPartner) {
   return camelizeObjectKeys(businessPartner)
 }
 
-export function convertConversionRate(conversionRateToConvert) {
-  const { id, uuid } = conversionRateToConvert
-  if (isEmptyValue(conversionRateToConvert.currency_from) && isEmptyValue(conversionRateToConvert.currency_from)) {
-    return {
-      uuid,
-      id,
-      multiplyRate: conversionRateToConvert.multiply_rate,
-      divideRate: conversionRateToConvert.divide_rate
-    }
+export function convertConversionRate(conversionRate) {
+  const convertedRate = camelizeObjectKeys(conversionRate)
+  if (isEmptyValue(conversionRate.currency_from) && isEmptyValue(conversionRate.currency_to)) {
+    delete convertedRate['validFrom']
+    delete convertedRate['conversionTypeUuid']
+    return convertedRate
   }
-  return {
-    uuid,
-    id,
-    conversionTypeUuid: conversionRateToConvert.conversion_type_uuid,
-    validFrom: conversionRateToConvert.valid_from,
-    currencyFrom: convertCurrency(
-      conversionRateToConvert.currency_from
-    ),
-    currencyTo: convertCurrency(
-      conversionRateToConvert.currency_to
-    ),
-    multiplyRate: conversionRateToConvert.multiply_rate,
-    divideRate: conversionRateToConvert.divide_rate
-  }
+  convertedRate.currencyFrom = convertCurrency(conversionRate.currency_from)
+  convertedRate.currencyTo = convertCurrency(conversionRate.currency_to)
+  return convertedRate
 }
 
-export function convertSalesRepresentative(salesRepresentativeToConvert) {
-  const { uuid, id, name, description } = salesRepresentativeToConvert
-
-  return {
-    uuid,
-    id,
-    name,
-    description
-  }
+export function convertSalesRepresentative(salesRepresentative) {
+  return camelizeObjectKeys(salesRepresentative)
 }
 
-export function convertBankAccount(bankAccountToConvert) {
-  if (!bankAccountToConvert) {
+export function convertBankAccount(bankAccount) {
+  if (!bankAccount) {
     return undefined
   }
-
-  const { uuid, id, name, description } = bankAccountToConvert
-  return {
-    uuid,
-    id,
-    name,
-    accountNo: bankAccountToConvert.account_no,
-    description,
-    currency: convertCurrency(
-      bankAccountToConvert.currency
-    ),
-    bban: bankAccountToConvert.bban,
-    iban: bankAccountToConvert.iban,
-    creditLimit: bankAccountToConvert.credit_limit,
-    current_balance: bankAccountToConvert.current_balance,
-    isDefault: bankAccountToConvert.is_default,
-    businessPartner: convertBusinessPartner(
-      bankAccountToConvert.business_partner
-    ),
-    bankAccountType: bankAccountToConvert.bank_account_type,
-    bankAccountTypeName: bankAccountToConvert.bank_account_type_name
-  }
+  const convertedBankAccount = camelizeObjectKeys(bankAccount)
+  convertedBankAccount.currency = convertCurrency(bankAccount.currency)
+  convertedBankAccount.businessPartner = convertBusinessPartner(bankAccount.business_partner)
+  renameObjectKey(convertedBankAccount, 'currentBalance', 'current_balance')
+  return convertedBankAccount
 }
 
-export function convertDocumentType(documentTypeToConvert) {
-  if (!documentTypeToConvert) {
+export function convertDocumentType(documentType) {
+  if (!documentType) {
     return undefined
   }
-  const { uuid, id, name, description } = documentTypeToConvert
-
-  return {
-    uuid,
-    id,
-    name,
-    description,
-    printName: documentTypeToConvert.print_name
-  }
+  return camelizeObjectKeys(documentType)
 }
 
-export function convertDocumentStatus(documentStatusToConvert) {
-  const { name, description } = documentStatusToConvert
-
-  return {
-    name,
-    description,
-    value: documentStatusToConvert.value
-  }
+export function convertDocumentStatus(documentStatus) {
+  return camelizeObjectKeys(documentStatus)
 }
 
-export function convertPriceList(priceListToConvert) {
-  const { uuid, id, name, description } = priceListToConvert
-
-  return {
-    uuid,
-    id,
-    name,
-    description,
-    currency: convertCurrency(
-      priceListToConvert.currency
-    ),
-    isDefault: priceListToConvert.is_default,
-    isTaxIncluded: priceListToConvert.is_tax_included,
-    isEnforcePriceLimit: priceListToConvert.is_enforce_price_limit,
-    isNetPrice: priceListToConvert.is_net_price,
-    pricePrecision: priceListToConvert.price_precision
-  }
+export function convertPriceList(priceList) {
+  const convertedPriceList = camelizeObjectKeys(priceList)
+  convertedPriceList.currency = convertCurrency(priceList.currency)
+  return convertedPriceList
 }
 
-export function convertProductPrice(productPriceToConvert) {
-  return {
-    currency: convertCurrency(
-      productPriceToConvert.currency
-    ),
-    taxRate: convertTaxRate(
-      productPriceToConvert.tax_rate
-    ),
-    product: convertProduct(
-      productPriceToConvert.product
-    ),
-    schemaCurrency: convertCurrency(
-      productPriceToConvert.schema_currency
-    ),
-    schemaPriceLimit: productPriceToConvert.schema_price_limit,
-    schemaPriceList: productPriceToConvert.schema_price_list,
-    schemaPriceStandard: productPriceToConvert.schema_price_standard,
-    priceList: productPriceToConvert.price_list,
-    priceStandard: productPriceToConvert.price_standard,
-    priceLimit: productPriceToConvert.price_limit,
-    priceListName: productPriceToConvert.price_list_name,
-    isTaxIncluded: productPriceToConvert.is_tax_included,
-    valid_from: productPriceToConvert.validFrom,
-    pricePrecision: productPriceToConvert.price_precision,
-    quantityOnHand: productPriceToConvert.quantity_on_hand,
-    quantityReserved: productPriceToConvert.quantity_reserved,
-    quantityOrdered: productPriceToConvert.quantity_ordered,
-    quantityAvailable: productPriceToConvert.quantity_available
-  }
+export function convertProductPrice(productPrice) {
+  const convertedProductPrice = camelizeObjectKeys(productPrice)
+  convertedProductPrice.currency = convertCurrency(productPrice.currency)
+  convertedProductPrice.taxRate = convertTaxRate(productPrice.tax_rate)
+  convertedProductPrice.product = convertProduct(productPrice.product)
+  convertedProductPrice.schemaCurrency = convertCurrency(productPrice.schema_currency)
+  renameObjectKey(convertedProductPrice, 'validFrom', 'valid_from')
+  return convertedProductPrice
 }
 
-export function convertTaxRate(taxRateToConvert) {
-  return {
-    name: taxRateToConvert.name,
-    description: taxRateToConvert.description,
-    taxIndicator: taxRateToConvert.tax_indicator,
-    rate: taxRateToConvert.rate
-  }
+export function convertTaxRate(taxRate) {
+  return camelizeObjectKeys(taxRate)
 }
 
-export function convertProduct(productToConvert) {
-  const { uuid, id, name, description, help } = productToConvert
-
-  return {
-    uuid,
-    id,
-    value: productToConvert.value,
-    name,
-    help,
-    documentNote: productToConvert.document_note,
-    uomName: productToConvert.uom_name,
-    productType: productToConvert.product_type,
-    isStocked: productToConvert.is_stocked,
-    isDropShip: productToConvert.is_drop_ship,
-    isPurchased: productToConvert.is_purchased,
-    isSold: productToConvert.is_sold,
-    imageUrl: productToConvert.image_url,
-    productCategoryName: productToConvert.product_category_name,
-    productGroupName: productToConvert.product_group_name,
-    productClassName: productToConvert.product_class_name,
-    productClassification_name: productToConvert.product_classification_name,
-    weight: productToConvert.weight,
-    volume: productToConvert.volume,
-    upc: productToConvert.upc,
-    sku: productToConvert.sku,
-    shelfWidth: productToConvert.shelf_width,
-    shelfHeight: productToConvert.shelf_height,
-    shelfDepth: productToConvert.shelf_depth,
-    unitsPerPack: productToConvert.units_per_pack,
-    unitsPerPallet: productToConvert.units_per_pallet,
-    guaranteeDays: productToConvert.guarantee_days,
-    descriptionUrl: productToConvert.description_url,
-    versionNo: productToConvert.version_no,
-    taxCategory: productToConvert.tax_category,
-    description
-  }
+export function convertProduct(product) {
+  const convertedProduct = camelizeObjectKeys(product)
+  renameObjectKey(convertedProduct, 'productClassificationName', 'productClassification_name')
+  return convertedProduct
 }
--- tests/unit/utils/ADempiere/apiConverts/core.spec.js
+++ tests/unit/utils/ADempiere/apiConverts/core.spec.js
@@ -1,62 +1,174 @@
-import { convertContextInfo, convertOrganization, convertLanguage, convertCountry, convertBusinessPartner } from '@/utils/ADempiere/apiConverts/core.js';
-import contextInfo from './objects/contextInfo.json'
-import convertedContextInfo from './objects/contextInfo_converted.json'
-import contextInfoWithoutMessage from './objects/contextInfoWithoutMessage.json'
-import convertedContextInfoWithoutMessage from './objects/contextInfoWithoutMessage_converted.json'
-import organization from './objects/organization.json'
-import convertedOrganization from './objects/organization_converted.json'
-import language from './objects/language.json'
-import convertedLanguage from './objects/language_converted.json'
-import country from './objects/country.json'
-import convertedCountry from './objects/country_converted.json'
-import countryWithNoCurrency from './objects/countryNoCurrency.json'
-import convertedCountryWithNoCurrency from './objects/countryNoCurrency_converted.json'
-import businessPartner from './objects/businessPartner.json'
-import convertedBusinessPartner from './objects/businessPartner_converted.json'
+import {
+  convertContextInfo,
+  convertOrganization,
+  convertLanguage,
+  convertCountry,
+  convertBusinessPartner,
+  convertConversionRate,
+  convertBankAccount,
+  convertDocumentType,
+  convertDocumentStatus,
+  convertPriceList,
+  convertTaxRate,
+  convertProduct,
+  convertProductPrice
+} from "@/utils/ADempiere/apiConverts/core.js";
+import contextInfo from "./objects/contextInfo.json";
+import convertedContextInfo from "./objects/contextInfo_converted.json";
+import contextInfoWithoutMessage from "./objects/contextInfoWithoutMessage.json";
+import convertedContextInfoWithoutMessage from "./objects/contextInfoWithoutMessage_converted.json";
+import organization from "./objects/organization.json";
+import convertedOrganization from "./objects/organization_converted.json";
+import language from "./objects/language.json";
+import convertedLanguage from "./objects/language_converted.json";
+import country from "./objects/country.json";
+import convertedCountry from "./objects/country_converted.json";
+import countryWithNoCurrency from "./objects/countryNoCurrency.json";
+import convertedCountryWithNoCurrency from "./objects/countryNoCurrency_converted.json";
+import businessPartner from "./objects/businessPartner.json";
+import convertedBusinessPartner from "./objects/businessPartner_converted.json";
+import conversionRate from "./objects/conversionRate.json";
+import convertedConversionRate from "./objects/conversionRate_converted.json";
+import conversionRateNoCurrency from "./objects/conversionRateNoCurrency.json";
+import convertedConversionRateNoCurrency from "./objects/conversionRateNoCurrency_converted.json";
+import salesRepresentative from "./objects/salesRepresentative.json";
+import convertedSalesRepresentative from "./objects/salesRepresentative_converted.json";
+import bankAccount from "./objects/bankAccount.json";
+import convertedBankAccount from "./objects/bankAccount_converted.json";
+import documentType from "./objects/documentType.json";
+import convertedDocumentType from "./objects/documentType_converted.json";
+import documentStatus from "./objects/documentStatus.json";
+import convertedDocumentStatus from "./objects/documentStatus_converted.json";
+import priceList from "./objects/priceList.json";
+import convertedPriceList from "./objects/priceList_converted.json";
+import taxRate from "./objects/taxRate.json";
+import convertedTaxRate from "./objects/taxRate_converted.json";
+import product from "./objects/product.json";
+import convertedProduct from "./objects/product_converted.json";
+import productPrice from "./objects/productPrice.json";
+import convertedProductPrice from "./objects/productPrice_converted.json";
 
-describe('context info', () => {
-    it('should return a converted context info', () => {
-        const actualContextInfo = convertContextInfo(contextInfo)
-        expect(actualContextInfo).toEqual(convertedContextInfo)
-    })
-    it('should return empty object for undefined context info', () => {
-        const actualContextInfo = convertContextInfo(null)
-        expect(actualContextInfo).toEqual({messageText: {}})
-    })
-    it('should return a converted context info with an empty message', () => {
-        const actualContextInfo = convertContextInfo(contextInfoWithoutMessage)
-        expect(actualContextInfo).toEqual(convertedContextInfoWithoutMessage)
-    })
-})
+describe("context info", () => {
+  it("should return a converted context info", () => {
+    const actualContextInfo = convertContextInfo(contextInfo);
+    expect(actualContextInfo).toEqual(convertedContextInfo);
+  });
+  it("should return empty object for undefined context info", () => {
+    const actualContextInfo = convertContextInfo(null);
+    expect(actualContextInfo).toEqual({ messageText: {} });
+  });
+  it("should return a converted context info with an empty message", () => {
+    const actualContextInfo = convertContextInfo(contextInfoWithoutMessage);
+    expect(actualContextInfo).toEqual(convertedContextInfoWithoutMessage);
+  });
+});
 
-describe('organization', () => {
-    it('should return a converted organization object', () => {
-        const actualOrganization = convertOrganization(organization)
-        expect(actualOrganization).toEqual(convertedOrganization)
-    })
-})
+describe("organization", () => {
+  it("should return a converted organization object", () => {
+    const actualOrganization = convertOrganization(organization);
+    expect(actualOrganization).toEqual(convertedOrganization);
+  });
+});
 
-describe('language', () => {
-    it('should return a converted language object', () => {
-        const actualLanguage = convertLanguage(language)
-        expect(actualLanguage).toEqual(convertedLanguage)
-    })
-})
+describe("language", () => {
+  it("should return a converted language object", () => {
+    const actualLanguage = convertLanguage(language);
+    expect(actualLanguage).toEqual(convertedLanguage);
+  });
+});
 
-describe('country', () => {
-    it('should return a converted country object with currency', () => {
-        const actualCountry = convertCountry(country)
-        expect(actualCountry).toEqual(convertedCountry)
-    })
-    it('should return a converted country object without currency', () => {
-        const actualCountry = convertCountry(countryWithNoCurrency)
-        expect(actualCountry).toEqual(convertedCountryWithNoCurrency)
-    })
-})
+describe("country", () => {
+  it("should return a converted country object with currency", () => {
+    const actualCountry = convertCountry(country);
+    expect(actualCountry).toEqual(convertedCountry);
+  });
+  it("should return a converted country object without currency", () => {
+    const actualCountry = convertCountry(countryWithNoCurrency);
+    expect(actualCountry).toEqual(convertedCountryWithNoCurrency);
+  });
+});
 
-describe('business partner', () => {
-    it('should return a converted business partner object', () => {
-        const actualBusinessPartner = convertBusinessPartner(businessPartner)
-        expect(actualBusinessPartner).toEqual(convertedBusinessPartner)
-    })
-})
+describe("business partner", () => {
+  it("should return a converted business partner object", () => {
+    const actualBusinessPartner = convertBusinessPartner(businessPartner);
+    expect(actualBusinessPartner).toEqual(convertedBusinessPartner);
+  });
+});
+
+describe("conversion rate", () => {
+  it("should return a converted conversion rate object with currencies", () => {
+    const actualConversionRate = convertConversionRate(conversionRate);
+    expect(actualConversionRate).toEqual(convertedConversionRate);
+  });
+
+  it("should return a converted conversion rate object without currencies", () => {
+    const actualConversionRate = convertConversionRate(
+      conversionRateNoCurrency
+    );
+    expect(actualConversionRate).toEqual(convertedConversionRateNoCurrency);
+  });
+});
+
+describe("sales representative", () => {
+  it("should return a converted sales representative object", () => {
+    const actualSalesRep = convertBusinessPartner(salesRepresentative);
+    expect(actualSalesRep).toEqual(convertedSalesRepresentative);
+  });
+});
+
+describe("bank account", () => {
+  it("should return a converted bank account object", () => {
+    const actualBankAccount = convertBankAccount(bankAccount);
+    expect(actualBankAccount).toEqual(convertedBankAccount);
+  });
+  it("should return undefined", () => {
+    const actualBankAccount = convertBankAccount(null);
+    expect(actualBankAccount).toBeUndefined();
+  });
+});
+
+describe("document type", () => {
+  it("should return a converted document type object", () => {
+    const actualDocumentType = convertDocumentType(documentType);
+    expect(actualDocumentType).toEqual(convertedDocumentType);
+  });
+  it("should return undefined", () => {
+    const actualDocumentType = convertBankAccount(null);
+    expect(actualDocumentType).toBeUndefined();
+  });
+});
+
+describe("document status", () => {
+  it("should return a converted document status object", () => {
+    const actualDocumentStatus = convertDocumentStatus(documentStatus);
+    expect(actualDocumentStatus).toEqual(convertedDocumentStatus);
+  });
+});
+
+describe("price list", () => {
+  it("should return a converted price list object", () => {
+    const actualPriceList = convertPriceList(priceList);
+    expect(actualPriceList).toEqual(convertedPriceList);
+  });
+});
+
+describe("tax rate", () => {
+  it("should return a converted tax rate object", () => {
+    const actualTaxRate = convertTaxRate(taxRate);
+    expect(actualTaxRate).toEqual(convertedTaxRate);
+  });
+});
+
+describe("product", () => {
+  it("should return a converted product object", () => {
+    const actualProduct = convertProduct(product);
+    expect(actualProduct).toEqual(convertedProduct);
+  });
+});
+
+describe("product price", () => {
+  it("should return a converted product price object", () => {
+    const actualProductPrice = convertProductPrice(productPrice);
+    expect(actualProductPrice).toEqual(convertedProductPrice);
+  });
+});
